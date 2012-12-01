c0d1c94699fb46003559bdf34cb24deabcdebb3c
hugh fd jackson <hughfdjackson@googlemail.com>
2012-12-01 16:24:58 -0500

    making readme for 0.1.0 release

---
+++ README.md
@@ -0,0 +1,39 @@
+# Tap 
+
+A mixin for seamless function and method interop. 
+
+## 
+
+
+## Installation 
+
+Available to download at 
+
+
+## More
+
+
+
+- see hughfdjackson.com/javascript/2012/11/30/tapping-into-the-method-chain/",
+  "main": "tap.js",
+  "scripts": {
+    "test": "mocha --ui qunit"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/hughfdjackson/tap.git"
+  },
+  "devDependencies": {
+    "underscore": "1.4.2"
+  },
+  "keywords": [
+    "function",
+    "functional",
+    "object",
+    "orientated",
+    "sugar",
+    "evil"
+  ],
+  "author": "hughfdjackson",
+  "license": "BSD"
+}
---
+++ tap.js
@@ -0,0 +1,22 @@
+void function(root){
+
+    var slice = [].slice
+
+    var tap = function(fn){
+        var val = this.valueOf()
+
+        if ( arguments.length == 1 ) return fn(val)
+
+        var args = slice.call(arguments, 1)
+        return fn.apply(null, [val].concat(args))
+    }
+
+    tap.mixin = function(o){
+        if ( Object.defineProperty ) Object.defineProperty(o, 'tap', { enumerable: false, value: tap, configurable: true, writable: true })
+        else                         o.tap = tap
+        return o
+    }
+
+    if ( typeof module == 'undefined' || module.exports == 'undefined' ) root.tap = tap
+    else                                                                 module.exports = tap
+}(this)
